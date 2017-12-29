a61896954b8059a135e78281bd1fa520d8bd9490
Alex Efros <powerman@powerman.name>
2017-12-29 13:42:59 +0200

    rename to Func and return *exec.Cmd

--- testexec/testexec.go
+++ testexec/testexec.go
@@ -10,37 +10,35 @@ import (
 
 var calledFrom = map[testing.TB]bool{}
 
-// Call let you test functions which may call os.Exit() or hang (for ex.
+// Func let you test functions which may call os.Exit() or hang (for ex.
 // main()) by running them in separate process. Result of executing f
 // should be returned by process exit status and/or output to
 // stdout/stderr. If f won't call os.Exit then os.Exit(0) will be called
 // when f returns.
 //
 //	package main
+//	var ctx = context.Background()
 //	func TestFlagHelp(tt *testing.T) {
 //		t := check.T(tt)
-//		out, err := testexec.Call(nil, t, main, "-h")
+//		out, err := testexec.Func(ctx, t, main, "-h").CombinedOutput()
 //		t.Match(err, "exit status 2")
 //		t.Match(out, "-version")
 //	}
 //
-// If you call Call twice per one test function it'll panic.
+// If you call Func twice per one test function it'll panic.
 //
-// Started process will execute same test function which calls Call up to
+// Started process will execute same test function which calls Func up to
 // this call (so this code is shared by both processes), then call f and
 // exits.
 //
 // Each subtest started with t.Run() counts as unique test function, but
 // beware subtests with t.Parallel() - they'll run after surrounding test
 // function returns, so in this case both processes will share not only
-// code before Call, but also code in surrounding function after t.Run()
-// containing this Call.
-//
-// Call returns the same as (*exec.Cmd).CombinedOutput() except it convert
-// stdoutStderr from []byte to string.
-func Call(ctx context.Context, t testing.TB, f func(), args ...string) (stdoutStderr string, err error) {
+// code before Func, but also code in surrounding function after t.Run()
+// containing this Func.
+func Func(ctx context.Context, t testing.TB, f func(), args ...string) *exec.Cmd {
 	if calledFrom[t] {
-		panic("Call can be used only once per test")
+		panic("Func can be used only once per test")
 	}
 	calledFrom[t] = true
 
@@ -52,6 +50,5 @@ func Call(ctx context.Context, t testing.TB, f func(), args ...string) (stdoutSt
 	args = append([]string{"-test.run=^" + regexp.QuoteMeta(t.Name()) + "$"}, args...)
 	cmd := exec.CommandContext(ctx, os.Args[0], args...)
 	cmd.Env = append(os.Environ(), "GO_WANT_HELPER_PROCESS=1")
-	out, err := cmd.CombinedOutput()
-	return string(out), err
+	return cmd
 }
